b40edd482e2ead67d4c7fb0b54aff5021679eeb0
yywjk-dmn <2986567641@qq.com>
2023-12-11 09:28:06 +0800

    [新增]新增一个算法：找到随机数组中两个数相加等于输入的数，并且输出索引

---
+++ twoNumAdd.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+#define MAXSIZE 20    //生成多大的数组
+#define RANGE 100    //生成随机数的大小
+
+int main()
+{
+    srand(time(NULL));
+    int randomArray[MAXSIZE];
+    memset(randomArray, 0, sizeof(randomArray));
+    int target;
+    for (int idx = 0; idx < MAXSIZE; idx++)     //生成随机数组
+    {
+        int num = rand() % RANGE + 1;     //生成1-100的随机数
+        randomArray[idx] = num;       //将生成的随机数放在数组里
+        //printf("%d\t",randomArray[idx]);
+    }
+    printf("请输入一个两个数相加的和：");
+    scanf("%d",&target);
+    int sum = 0;  //和
+    int firstNum,secondNum;;    //开始遍历的数字
+    int idx,index;      //索引
+    int flag = 0;
+    for(idx = 0; idx < MAXSIZE; idx++)
+    {
+        for (index = idx + 1; index < MAXSIZE; index++)
+        {
+            firstNum = randomArray[idx];
+            secondNum = randomArray[index];
+            sum = firstNum + secondNum;        //将两个数相加
+            if (sum = target)
+            {
+                flag = 1;
+                printf("随机数组中相加等于target的数的索引是:%d %d\n", idx, index);
+            }
+        }
+    }
+    if(flag = 0)
+    {
+        printf("没有找到相加等于target的这两个数\n");
+    }
+
+    return 0;
+}
